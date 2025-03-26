d8ac865d198da9e541938307615004bbdd177f4a
LekhaBollineni <lekhabollineni2022@gmail.com>
2025-03-26 15:43:22 -0400

    basic blog structure created

--- MyBlogProject/blog/urls.py
+++ MyBlogProject/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='blog_index'),
+    path('<slug:slug>', views.blog_detail, name='blog_detail'),
 ]
--- MyBlogProject/blog/views.py
+++ MyBlogProject/blog/views.py
@@ -2,5 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#Declaring a class BlogPost with two attributes: title and content
+class BlogPost:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+#Creating a list of BlogPost objects
+blog_posts = [
+    BlogPost('First Post', 'This is my first blog post.'),
+    BlogPost('Second Post', 'This is my second blog post.'),
+    BlogPost('Third Post', 'This is my third blog post.'),
+]
+
+#Creating a function index that returns the index.html template
 def index(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/index.html', {'blog_posts': blog_posts})
+        
+
+def blog_detail(request, slug):
+    return render(request,'blog/blog_post.html',{'blog_title':slug})
+
